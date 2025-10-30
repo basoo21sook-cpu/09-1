66162ae54d665d5caa8bc0ae05bd5a1380ea2c3b
basoo21sook-cpu <basoo21sook@gmail.com>
2025-10-30 15:41:49 +0900

    09-2

--- main.c
+++ main.c
@@ -3,20 +3,26 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+#include <stdio.h>
+#define SIZE 5   // 상수 정의 (배열 크기)
+
 int main(void)
 {
-    int i;
-    int grade[5];
-
-    grade[0] = 10;
-    grade[1] = 20;
-    grade[2] = 30;
-    grade[3] = 40;
-    grade[4] = 50;
-
-    for (i = 0; i < 5; i++)
-        printf("grade[%d] = %d\n", i, grade[i]);
+    int i,average;
+    int sum;
+    int grade[SIZE];
+	
+	sum=0;
+    for (i = 0; i <  SIZE; i++)
+    {
+    	 printf("학생 성적을 입력하세요:");
+		scanf("%d",&grade[i]);
+		sum+=grade[i];
+	}
+    average = sum / SIZE;
+    printf("성적 평균 : %d\n",average);
 
     return 0;
 }
+
 
